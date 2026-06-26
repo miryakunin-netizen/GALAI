5d2adaf8535889c218db611e92285ff0484da649
miryakunin-netizen <mir.yakunin@gmail.com>
2026-06-26 17:40:13 +0700

    Update index.js

--- server/tools/index.js
+++ server/tools/index.js
@@ -1,1 +1,20 @@
+export const ToolRegistry = {
 
+    tools: {},
+
+    register(name, tool) {
+        this.tools[name] = tool;
+    },
+
+    has(name) {
+        return !!this.tools[name];
+    },
+
+    get(name) {
+        return this.tools[name];
+    },
+
+    list() {
+        return Object.keys(this.tools);
+    }
+};
