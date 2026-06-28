76fb71e65b9f3ddfb8102ceb08a760cee38ba8ed
miryakunin-netizen <mir.yakunin@gmail.com>
2026-06-28 13:58:27 +0700

    Update pdfExtractor.js

--- server/extractors/pdfExtractor.js
+++ server/extractors/pdfExtractor.js
@@ -1,11 +1,11 @@
-import pdf from "pdf-parse";
+import pdfParse from "pdf-parse";
 
 export async function extractPdf(buffer) {
-    const data = await pdf(buffer);
+    const data = await pdfParse(buffer);
 
     return {
         text: data.text,
         pages: data.numpages,
-        info: data.info
+        info: data.info ?? {}
     };
 }
