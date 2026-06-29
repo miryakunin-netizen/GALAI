de39893c81cc9ecb337f457752a18ac722c59f7f
miryakunin-netizen <mir.yakunin@gmail.com>
2026-06-29 10:46:20 +0700

    Create extractorService.js

---
+++ server/services/extractorService.js
@@ -0,0 +1,22 @@
+import { extractPdf } from "../extractors/pdfExtractor.js";
+
+export async function extractDocument(file) {
+
+    if (!file) {
+        throw new Error("Файл отсутствует");
+    }
+
+    const type = file.mimetype.toLowerCase();
+
+    if (
+        type === "application/pdf" ||
+        file.originalname.toLowerCase().endsWith(".pdf")
+    ) {
+        return await extractPdf(file.buffer);
+    }
+
+    throw new Error(
+        `Формат пока не поддерживается: ${type}`
+    );
+
+}
